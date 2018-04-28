2e03f6e14f6dde957261699597ec25ef9ff3a54c
KimSia Sim <kimcity@gmail.com>
2018-04-28 19:52:00 +0800

    DRF works with DJA using /users

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from polls import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
