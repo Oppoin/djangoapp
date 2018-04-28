d531a93e16f3aa8c1bfe4215237e9a4706ed57c7
KimSia Sim <kimcity@gmail.com>
2018-04-28 19:30:40 +0800

    changes so that we have 2 users and we add serializers

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from web import views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
--- web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiplaceholderWebConfig(AppConfig):
-    name = 'apiplaceholder_web'
